65c274b48e39d30215a430e50c97a0f6dde51bf9
aydian-dev <ayda_hadadian@yahoo.com>
2022-08-21 19:46:55 +0430

    Weather section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getWeatherData } from "./api/weatherApi";
 import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
 
 
 export default function App() {
+const[weatherData, setWeatherData] = useState([]);
+
+  // useEffect(() => {
+
+
+  //    getWeatherData(51.4215,35.6944)
+  //       .then((data)=> {
+       
+  //         setWeatherData(data[0]);
+  //         console.log("heoooo");
+  //       })
+       
+
+  // }, [])
+
+
+  
   return (
     <Grid container flexWrap="nowrap">
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
 import { Grid } from '@mui/material';
 import Navbar from './Navbar';
+import Weather from './Weather';
 
 const Dashboard = () => {
   return (
-    <Grid item xs={9.5}>
+    <Grid item xs={9.5} px={1}>
       
     <Navbar />
+    <Weather />
 
     </Grid>
   
